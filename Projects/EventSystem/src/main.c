c67562846c05a8673fc8f9e46c653c866565946d
Roberto Duarte <robertodanielalvesduarte@gmail.com>
2019-10-16 16:28:59 +0100

    Minor changes on EventSystem project.

--- Projects/EventSystem/src/main.c
+++ Projects/EventSystem/src/main.c
@@ -1,8 +1,23 @@
-#include "game.h"
+#include <sgl.h>
+#include "Utils/mem_mgr.h"
+
+void init()
+{
+    slInitSystem(TV_320x240, NULL, 1);
+    slPerspective(DEGtoANG(60.0));
+
+    // use 512K of work RAM for general heap
+    // handle set size = 16, trash RAM = 2K
+    MemInit((pointer)0x06040000, (pointer)0x060BFFFF, 16, 16, 0x800);
+}
 
 int main(void)
 {
-    Game_init();
-    while (Game_run());
+    init();
+    while (1)
+    {
+        slPrint("Just WORKS!", slLocate(0, 0));
+        slSynch();
+    }
     return 1;
 }
