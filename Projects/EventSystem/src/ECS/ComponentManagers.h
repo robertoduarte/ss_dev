96d2e3b3fe73a701c696179ce6fe362bb42ddd34
Roberto Duarte <robertodanielalvesduarte@gmail.com>
2019-12-03 00:34:25 +0000

    Added alternative component definition draft.

--- Projects/EventSystem/src/ECS/ComponentManagers.h
+++ Projects/EventSystem/src/ECS/ComponentManagers.h
@@ -2,31 +2,9 @@
 #define HEALTH_MANAGER_H
 #include "ComponentManagerTemplate.h"
 
-typedef struct
-{
-    Sint16 entityId;
-    Uint16 currentHealth;
-    Uint16 maxHealth;
-} Health;
-
 COMPONENT_MANAGER_HEADER(Health)
 
-typedef struct
-{
-    Sint16 entityId;
-    POINT x;
-    POINT y;
-    POINT z;
-} Transform;
-
 COMPONENT_MANAGER_HEADER(Transform)
-
-typedef struct
-{
-    Sint16 entityId;
-    VECTOR velocity;
-    VECTOR acceleration;
-} Motion;
 
 COMPONENT_MANAGER_HEADER(Motion)
 
--- Projects/EventSystem/src/ECS/EntitySystem.h
+++ Projects/EventSystem/src/ECS/EntitySystem.h
@@ -1,20 +1,13 @@
 #ifndef ENTITY_SYSTEM_H
 #define ENTITY_SYSTEM_H
 #include "SL_DEF.H"
+#include "ComponentTypes.h"
 
 #define MAX_ENTITIES 100
 #define UNUSED_COMPONENT -1
 
 #define EntityId Sint16
 #define ComponentId Sint16
-
-typedef enum
-{
-    HEALTH_COMPONENT,
-    TRANSFORM_COMPONENT,
-    MOTION_COMPONENT,
-    COMPONENT_COUNT
-} ComponentType;
 
 inline void EntitySystem_Initialize();
 
